933c096247466ebc862c0579ee5b100c151a5a1e
freddyasp21 <freddyperozo21@gmail.com>
2023-04-11 02:37:51 -0400

    update

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='h-[7vh] md:h-[10vh]'>Header</header>
+  )
+}
+
+export default Header
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='xl:h-[100vh] overflow-y-scroll fixed xl:static w-full h-full top-0 -left-full'>Sidebar</div>
+  )
+}
+
+export default Sidebar
