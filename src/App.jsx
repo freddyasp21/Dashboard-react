04197f03ccff8e78d3543254ad71707896b7a15a
freddyasp21 <freddyperozo21@gmail.com>
2023-04-10 20:33:01 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Layout
+import LayoutAdmin from "./layouts/LayoutAdmin";
+import LayoutAuth from "./layouts/LayoutAuth";
+// Pages auth
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+
+// Pages admin
+import Home from "./pages/admin/Home";
+import Chat from './pages/admin/Chat'
+import Error404 from "./pages/Erro404";
 
 function App() {
   return (
-    <div className='bg-red-300'>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<LayoutAuth/>}>
+          <Route index element={<Login />} />
+          <Route path="registro" element={<Register />} />
+        </Route>
+        <Route path="/" element={<LayoutAdmin />}>
+          <Route index element={<Home />} />
+          <Route path="chat" element={<Chat/>}/>
+        </Route>
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Erro404.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Erro404() {
+  return (
+    <div>Erro404</div>
+  )
+}
+
+export default Erro404
