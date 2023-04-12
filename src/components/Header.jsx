707b45440f3dc175131d9c7a00c9de336ccdbef1
freddyasp21 <freddyperozo21@gmail.com>
2023-04-11 22:36:51 -0400

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-import { RiNotification2Line, RiArrowDownSLine, RiSettings3Line, RiMessage3Line, RiMoneyDollarCircleLine, RiWalletLine, RiLoginCircleLine, } from "react-icons/ri";
+import {
+  RiNotification2Line,
+  RiArrowDownSLine,
+  RiSettings3Line,
+  RiMessage3Line,
+  RiMoneyDollarCircleLine,
+  RiWalletLine,
+  RiLoginCircleLine,
+  RiHeart3Fill,
+  RiThumbUpLine,
+} from "react-icons/ri";
 import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
 import "@szhsin/react-menu/dist/index.css";
 import "@szhsin/react-menu/dist/transitions/slide.css";
@@ -9,12 +19,65 @@ function Header() {
   return (
     <header className="h-[7vh] md:h-[10vh] border-b border-secondary-100 p-8 flex items-center justify-end">
       <nav className="flex items-center">
-        <button className="relative hover:bg-secondary-100 p-2 rounded-lg transition-colors">
-          <RiNotification2Line className="text-white" />
-          <span className="absolute -top-0.5 right-0 py-0.5 px-[5px] box-content text-black bg-primary rounded-full text-[8px] font-bold">
-            2
-          </span>
-        </button>
+        <Menu
+          menuButton={
+            <MenuButton className="flex items-center gap-x-2 relative hover:bg-secondary-100 py-2 px-4 rounded-lg transition-colors">
+              <RiNotification2Line className="text-white" />
+              <span className="absolute -top-0.5 right-2 py-0.5 px-[5px] box-content text-black bg-primary rounded-full text-[8px] font-bold">
+                3
+              </span>
+            </MenuButton>
+          }
+          transition
+          menuClassName="bg-secondary-100 text-white p-4"
+          arrow={true}
+          arrowClassName="bg-secondary-100"
+          className="relative hover:bg-secondary-100 p-2 rounded-lg transition-colors"
+        >
+          <h2 className=" px-4 py-2 text-lg text-primary text-center font-bold mb-4">
+            Notificaciones
+          </h2>
+          <MenuItem className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 mb-2">
+            <Link to="/" className="flex items-center gap-4 px-2 py-2 text-sm">
+              <img
+                src="https://img.freepik.com/foto-gratis/retrato-frontal-mujer-rostro-belleza_186202-6146.jpg"
+                alt=""
+                className="w-8 h-8 object-cover rounded-full"
+              />
+              <div className="flex flex-col">
+                <div className="flex items-center justify-between">
+                  <span className="text-primary text-sm">Luis Daniel</span>{" "}
+                  <span className="text-[8px]">11/4/2023</span>
+                </div>
+                <p className="text-gray-500">Lorem ipsum dolor sit...</p>
+              </div>
+            </Link>
+          </MenuItem>
+          <MenuItem className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 mb-2">
+            <Link to="/" className="flex items-center gap-4 px-2 py-2 text-sm">
+              <RiHeart3Fill className="text-red-800 text-3xl" />
+              <div className="flex flex-col">
+                <div className="flex items-center justify-between">
+                  <span className="text-primary text-sm">Julieta Medina</span>{" "}
+                  <span className="text-[8px]">11/4/2023</span>
+                </div>
+                <p className="text-gray-500">Lorem ipsum dolor sit...</p>
+              </div>
+            </Link>
+          </MenuItem>
+          <MenuItem className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 mb-2">
+            <Link to="/" className="flex items-center gap-4 px-2 py-2 text-sm">
+              <RiThumbUpLine className="text-[#3b5998] text-3xl" />
+              <div className="flex flex-col">
+                <div className="flex items-center justify-between">
+                  <span className="text-primary text-sm">Paul Phoenix</span>{" "}
+                  <span className="text-[8px]">11/4/2023</span>
+                </div>
+                <p className="text-gray-500">Lorem ipsum dolor sit...</p>
+              </div>
+            </Link>
+          </MenuItem>
+        </Menu>
         <Menu
           menuButton={
             <MenuButton className="flex items-center gap-x-2 relative hover:bg-secondary-100 py-2 px-4 rounded-lg transition-colors">
@@ -29,10 +92,12 @@ function Header() {
             </MenuButton>
           }
           transition
+          arrow={true}
+          arrowClassName="bg-secondary-100"
           menuClassName="bg-secondary-100 text-white p-4"
         >
           {" "}
-          <MenuItem className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 mb-2">
+          <MenuItem className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 mb-4">
             <Link to="/perfil" className="flex items-center gap-x-4">
               <img
                 src="https://img.freepik.com/foto-gratis/concepto-emociones-personas-foto-cabeza-hombre-guapo-aspecto-serio-barba-confiado-decidido_1258-26730.jpg"
@@ -49,20 +114,33 @@ function Header() {
               </div>
             </Link>
           </MenuItem>
-          <MenuItem className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 mb-2">
-            <Link to="/configuracion" className="flex items-center gap-2"><RiSettings3Line className="text-primary"/> Configuración</Link>
+          <MenuItem className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 my-2">
+            <Link
+              to="/configuracion"
+              className="flex items-center gap-2 px-4 py-2"
+            >
+              <RiSettings3Line className="text-primary" /> Configuración
+            </Link>
           </MenuItem>
-          <MenuItem className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 mb-2">
-            <Link to="/mensajes" className="flex items-center gap-2"><RiMessage3Line className="text-primary"/> Mensajes</Link>
+          <MenuItem className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 my-2">
+            <Link to="/mensajes" className="flex items-center gap-2 px-4 py-2">
+              <RiMessage3Line className="text-primary" /> Mensajes
+            </Link>
           </MenuItem>
-          <MenuItem className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 mb-2">
-            <Link to="/ganancias" className="flex items-center gap-2"><RiMoneyDollarCircleLine  className="text-primary"/> Ganancias</Link>
+          <MenuItem className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 my-2">
+            <Link to="/ganancias" className="flex items-center gap-2 px-4 py-2">
+              <RiMoneyDollarCircleLine className="text-primary" /> Ganancias
+            </Link>
           </MenuItem>
-          <MenuItem className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 mb-2">
-            <Link to="/wallet" className="flex items-center gap-2"><RiWalletLine  className="text-primary"/> Wallet</Link>
+          <MenuItem className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 my-2">
+            <Link to="/wallet" className="flex items-center gap-2 px-4 py-2">
+              <RiWalletLine className="text-primary" /> Wallet
+            </Link>
           </MenuItem>
-          <MenuItem className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 mb-2">
-            <Link to="/logout" className="flex items-center gap-2"><RiLoginCircleLine  className="text-primary"/> Cerrar sesión</Link>
+          <MenuItem className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 my-2">
+            <Link to="/logout" className="flex items-center gap-2 px-4 py-2">
+              <RiLoginCircleLine className="text-primary" /> Cerrar sesión
+            </Link>
           </MenuItem>
         </Menu>
       </nav>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import {
   RiBarChart2Fill,
   RiEarthLine,
-  RiHeadphoneLine,
+  RiCustomerService2Line,
   RiCalendar2Line,
   RiLoginCircleLine,
   RiArrowDownSLine,
@@ -89,7 +89,7 @@ function Sidebar() {
                 className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
                 to="/"
               >
-                <RiHeadphoneLine className="text-primary" /> Soporte Técnico
+                <RiCustomerService2Line className="text-primary" /> Soporte Técnico
               </Link>
             </li>
             <li className="">
