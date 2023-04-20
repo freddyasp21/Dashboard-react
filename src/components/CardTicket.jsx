7084edc87afa058dcf8cb54f39a427d49a590733
freddyasp21 <freddyperozo21@gmail.com>
2023-04-19 23:00:17 -0400

    update

--- src/components/CardTicket.jsx
+++ src/components/CardTicket.jsx
@@ -1,43 +1,48 @@
 import React from "react";
-import { RiTicketLine, RiMore2Line } from "react-icons/ri";
+import {
+  RiTicketLine,
+  RiMore2Line,
+  RiMoneyDollarCircleLine,
+  RiBarChart2Line,
+  RiCalendarLine
+} from "react-icons/ri";
 import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
 import "@szhsin/react-menu/dist/index.css";
 import "@szhsin/react-menu/dist/transitions/slide.css";
 import { Link } from "react-router-dom";
 
 function CardTickets(props) {
-  const { ticket, totalTickets, text } = props;
+  const { icon, mainText, text} = props;
 
   let status = "";
 
-  switch (ticket) {
-    case "pending":
-      status = "bg-yellow-500/10 text-yellow-500";
+  switch (icon) {
+    case "ticket":
+      status = <RiTicketLine className="text-4xl bg-pink-500/10 text-pink-500 p-2 box-content rounded-xl" />;
       break;
 
-    case "inProcess":
-      status = "bg-blue-500/10 text-blue-500";
+    case "money":
+      status = <RiMoneyDollarCircleLine className="text-4xl bg-yellow-500/10 text-yellow-500 p-2 box-content rounded-xl" />;
       break;
 
-    case "inProcess":
-      status = "bg-green-500/10 text-green-500";
+    case "chart":
+      status = <RiBarChart2Line className="text-4xl bg-blue-500/10 text-blue-500 p-2 box-content rounded-xl" />;
       break;
 
-    case "total":
-      status = "bg-pink-500/10 text-pink-500";
+    case "calendar":
+      status = <RiCalendarLine className="text-4xl bg-primary/10 text-primary p-2 box-content rounded-xl" />;
       break;
 
     default:
       break;
   }
 
+
   return (
     <div className="bg-secondary-100 p-8 rounded-xl">
       <div className="flex items-center justify-between mb-4">
         <div className="">
-          <RiTicketLine
-            className={`text-4xl ${status} p-2 box-content rounded-xl`}
-          />
+        {status}
         </div>
         <div className="">
           <Menu
@@ -68,7 +73,7 @@ function CardTickets(props) {
       </div>
       {/* Number of Tickets */}
       <div className="">
-        <h3 className="text-4xl text-white font-bold mb-4">{totalTickets}</h3>
+        <h3 className="text-4xl text-white font-bold mb-4">{mainText}</h3>
         <p className="">{text}</p>
       </div>
     </div>
--- src/pages/admin/Home.jsx
+++ src/pages/admin/Home.jsx
@@ -6,7 +6,10 @@ function Home() {
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {/* Card */}
-        <CardTicket ticket="total" totalTickets="145.000" text="Total tickets"/>
+        <CardTicket icon="ticket" mainText="145.000" text="Total Tickets"/>
+        <CardTicket icon="money" mainText="80.000" text="Total Earnings"/>
+        <CardTicket icon="chart" mainText="20.000" text="Total Sales"/>
+        <CardTicket icon="calendar" mainText="202" text="Total Days of Work"/>
       </div>
     </div>
   );
